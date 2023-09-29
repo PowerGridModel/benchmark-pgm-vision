da05f5c55db2eff67bb6bdf67272641b545568ae
Tony Xiang <tony.xiang@alliander.com>
2023-09-29 10:22:04 +0200

    pgm does not converge
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from power_grid_model_io.converters import VisionExcelConverter
+from power_grid_model import PowerGridModel
 import os
 from pathlib import Path
+import pandas as pd
 
 
 vision_config = Path(__file__).parent / "vision_en.yaml"
@@ -14,3 +16,6 @@ converter = VisionExcelConverter(source_file=input_file)
 converter.set_mapping_file(vision_config)
 input_data, extra_info = converter.load_input_data()
 
+pgm = PowerGridModel(input_data)
+pgm_result = pgm.calculate_power_flow()
+print(pd.DataFrame(pgm_result["node"]))
