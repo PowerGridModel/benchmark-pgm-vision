924c691bf7c41c21be4201a448db7ba06d3b5ef5
Tony Xiang <tony.xiang@alliander.com>
2023-09-30 13:30:26 +0200

    check voltage not good
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 import pandas as pd
+import numpy as np
 from power_grid_model import PowerGridModel, initialize_array
 from power_grid_model.validation import assert_valid_input_data
 from power_grid_model_io.converters import VisionExcelConverter
@@ -51,6 +52,32 @@ def load_profile(input_data, extra_info, n_steps):
     return update_data
 
 
+def compare_result(pgm, pgm_result, extra_info, test_case, n_steps):
+    vision_result_file = DATA_PATH / "excel_result" / f"{test_case}.xlsx"
+    vision_df = pd.read_excel(
+        vision_result_file, sheet_name="Knooppunten", skiprows=[1, 2], nrows=n_steps
+    ).set_index("Naam")
+    vision_df.index.name = "Date & Time"
+    vision_df *= 1e3
+    node_names = vision_df.columns.to_list()
+    node_vision_name_to_pgm_dict = {
+        extra_info[x].get("Name", ""): x for x in pgm_result["node"][0, :]["id"]
+    }
+    vision_node_indexer = []
+    node_pgm_ids = []
+    for i, name in enumerate(node_names):
+        if name in node_vision_name_to_pgm_dict:
+            vision_node_indexer.append(i)
+            node_pgm_ids.append(node_vision_name_to_pgm_dict[name])
+    vision_node_indexer = np.array(vision_node_indexer, dtype=np.int64)
+    node_pgm_ids = np.array(node_pgm_ids, dtype=np.int32)
+    pgm_node_indexer = pgm.get_indexer("node", node_pgm_ids)
+    u_pgm = pgm_result["node"][:, pgm_node_indexer]["u"]
+    u_vision = vision_df.iloc[:, vision_node_indexer].to_numpy()
+    max_diff = np.max(np.abs(u_vision - u_pgm))
+    print(f"Max voltage deviation: {max_diff} V.")
+
+
 def main():
     test_case = "Leeuwarden_small_P"
     n_steps = 10
@@ -61,6 +88,8 @@ def main():
     pgm = PowerGridModel(input_data)
     pgm_result = pgm.calculate_power_flow(update_data=update_data)
     print(pd.DataFrame(pgm_result["node"][0, :]))
+
+    compare_result(pgm, pgm_result, extra_info, test_case, n_steps)
 
 
 if __name__ == "__main__":
