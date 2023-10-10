38ff3ca9b257ad6f7e4e461973314895a57aaddd
Tony Xiang <tony.xiang@alliander.com>
2023-10-10 16:00:06 +0200

    add result
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- convert.py
+++ convert.py
@@ -60,6 +60,43 @@ def load_profile(data_path, input_data, extra_info, n_steps):
     return update_data
 
 
+def get_id_map(extra_info):
+    id_map = {}
+    for i, info in extra_info.items():
+        ref = info["id_reference"]
+        name = ref["table"]
+        if "Subnumber" in ref["key"]:
+            sub_number = ref["key"]["Subnumber"]
+            node_number = ref["key"]["Node.Number"]
+            value = f"{name}.{node_number}.{sub_number}"
+        else:
+            number = ref["key"]["Number"]
+            value = f"{name}.{number}"
+        id_map[i] = value
+    return pd.Series(id_map)
+
+
+def load_vision_results(test_case, data_path, input_data, extra_info, n_steps):
+    result_path = data_path / "csv_result" / test_case
+    id_map = get_id_map(extra_info)
+    # node
+    node_df = pd.read_csv(result_path / "node.csv", skiprows=[2], nrows=n_steps, engine="c", header=[0, 1]).iloc[:, 1:]
+    node_df = node_df.loc[:, (slice(None), "U")].droplevel(1, axis=1)
+    node_df *= 1e3
+    cols = node_df.columns
+    node_df.columns = [f"Nodes.{x}" for x in cols]
+    node_ids = input_data["node"]["id"]
+    col_names = id_map.loc[input_data["node"]["id"]]
+    col_idx = node_df.columns.get_indexer(col_names)
+    node_found = col_idx >= 0
+    node_ids = node_ids[node_found]
+    col_idx = col_idx[node_found]
+    node_result = initialize_array("sym_output", "node", shape=(n_steps, node_ids.size))
+    node_result["id"] = node_ids.reshape(1, -1)
+    node_result["u"] = node_df.iloc[:, col_idx]
+    return {"node": node_result}
+
+
 class NpEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.integer):
@@ -71,11 +108,13 @@ class NpEncoder(json.JSONEncoder):
         return super(NpEncoder, self).default(obj)
 
 
-def dump_to_file(data_path: Path, test_case, input_data, update_data, extra_info):
+def dump_to_file(data_path: Path, test_case, input_data, update_data, result_data, extra_info):
     output_path: Path = data_path / "PGM_data" / test_case
     output_path.mkdir(parents=True, exist_ok=True)
     msgpack_serialize_to_file(file_path=output_path / "input.pgmb", data=input_data, use_compact_list=True)
     msgpack_serialize_to_file(file_path=output_path / "update.pgmb", data=update_data, use_compact_list=True)
+    msgpack_serialize_to_file(file_path=output_path / "result.pgmb", data=result_data, use_compact_list=True)
+
     with open(output_path / "extra_info.json", "w") as f:
         json.dump(extra_info, f, cls=NpEncoder, indent=2)
 
@@ -94,7 +133,9 @@ def main():
     input_data, extra_info = load_input(test_case, data_path)
     update_data = load_profile(data_path, input_data, extra_info, n_steps)
 
-    dump_to_file(data_path, test_case, input_data, update_data, extra_info)
+    result_data = load_vision_results(test_case, data_path, input_data, extra_info, n_steps)
+
+    dump_to_file(data_path, test_case, input_data, update_data, result_data, extra_info)
 
 
 if __name__ == "__main__":
