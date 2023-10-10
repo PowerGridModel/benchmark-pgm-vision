6844fbd8fc84ae59c06f1c670f7852886150eb87
Tony Xiang <tony.xiang@alliander.com>
2023-10-10 11:18:19 +0200

    run calculation
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- run.py
+++ run.py
@@ -5,12 +5,25 @@ import numpy as np
 from power_grid_model import PowerGridModel
 from power_grid_model.utils import msgpack_deserialize_from_file
 import argparse
+from time import time
 
 
-def run(input_data, update_data, calculation_method):
+def run(input_data, update_data, calculation_method, n_steps):
+    start = time()
     pgm = PowerGridModel(input_data)
+    end = time()
+    print(f"\n---Model initialization---")
+    print(f"Time cost: {end - start} seconds")
+    print("Components count")
+    print(pgm.all_component_count)
+    start = time()
     pgm_result = pgm.calculate_power_flow(update_data=update_data, calculation_method=calculation_method)
-    print("----Node Result---")
+    end = time()
+    print(f"\n---Calculation---")
+    print(f"Method: {calculation_method}")
+    print(f"Steps: {n_steps}")
+    print(f"Time cost: {end - start} seconds")
+    print("\n\n---Node Result---")
     print(pd.DataFrame(pgm_result["node"][0, :]).head())
     return pgm, pgm_result
 
@@ -22,8 +35,10 @@ def load_data(data_path, test_case, n_steps):
 
     if n_steps is not None:
         update_data = {k: v[:n_steps, ...] for k, v in update_data.items()}
+    else:
+        n_steps = next(iter(update_data.values())).shape[0]
 
-    return input_data, update_data
+    return input_data, update_data, n_steps
 
 
 # noinspection DuplicatedCode
@@ -40,9 +55,11 @@ def main():
     test_case = args.test_case
     calculation_method = args.calculation_method
 
-    input_data, update_data = load_data(data_path, test_case, n_steps)
+    print(f"Test case: {test_case}")
 
-    run(input_data, update_data, calculation_method)
+    input_data, update_data, n_steps = load_data(data_path, test_case, n_steps)
+
+    run(input_data, update_data, calculation_method, n_steps)
 
 
 if __name__ == "__main__":
