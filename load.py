5f58be41475222ab02d3a94ac10cc1b5a1c9dd3e
Tony Xiang <tony.xiang@alliander.com>
2023-11-05 13:29:49 +0100

    run load
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

---
+++ load.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from power_grid_model.utils import msgpack_deserialize_from_file
+
+from time import time
+
+
+path = Path(__file__).parent / "data" / "PGM_data" / "Leeuwarden_big_P" / "result.pgmb"
+
+file_size = path.stat().st_size / 1e9
+
+start = time()
+data = msgpack_deserialize_from_file(path)
+end = time()
+print(f"Time to load: {end - start} seconds.")
+
+buffer_size = 0
+for v in data.values():
+    buffer_size += v.itemsize * v.size / 1e9
+
+print(f"Memory usage of file size: {file_size} GB.")
+print(f"Memory usage of buffers: {buffer_size} GB.")
